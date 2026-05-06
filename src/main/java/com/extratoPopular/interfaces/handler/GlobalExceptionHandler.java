6ed10f9986987cd417eacb7e2843a9e37edd30e3
Joyce <joycejsm96@protonmail.com>
2026-05-06 07:28:40 -0300

    feat: implementa GlobalExceptionHandler para padronização de erros e documentação Swagger

--- src/main/java/com/extratoPopular/interfaces/handler/GlobalExceptionHandler.java
+++ src/main/java/com/extratoPopular/interfaces/handler/GlobalExceptionHandler.java
@@ -1,4 +1,60 @@
 package com.extratoPopular.interfaces.handler;
 
+import com.extratoPopular.domain.exception.CredenciaisInvalidasException;
+import com.extratoPopular.domain.exception.EmailJaCadastradoException;
+import com.extratoPopular.domain.exception.UsuarioNaoAutenticadoException;
+import com.extratoPopular.interfaces.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> detalhes = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> {
+                    if (error instanceof FieldError fieldError) {
+                        return fieldError.getField() + ": " + fieldError.getDefaultMessage();
+                    }
+                    return error.getDefaultMessage();
+                })
+                .collect(Collectors.toList());
+
+        return new ErrorResponse("VALIDACAO_FALHOU", detalhes);
+    }
+
+    @ExceptionHandler(EmailJaCadastradoException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleEmailJaCadastradoException(EmailJaCadastradoException ex) {
+        return new ErrorResponse("EMAIL_JA_CADASTRADO", null);
+    }
+
+    @ExceptionHandler(CredenciaisInvalidasException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleCredenciaisInvalidasException(CredenciaisInvalidasException ex) {
+        return new ErrorResponse("CREDENCIAIS_INVALIDAS", null);
+    }
+
+    @ExceptionHandler(UsuarioNaoAutenticadoException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleUsuarioNaoAutenticadoException(UsuarioNaoAutenticadoException ex) {
+        return new ErrorResponse("NAO_AUTENTICADO", null);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleGenericException(Exception ex) {
+        return new ErrorResponse("ERRO_INTERNO", null);
+    }
 }
