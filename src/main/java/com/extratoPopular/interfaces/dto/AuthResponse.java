d734b088d5e5c689dd8715da249bf33909d50c5d
Joyce <joycejsm96@protonmail.com>
2026-05-06 06:47:48 -0300

    feat: implementa Records de Auth com Bean Validation e documentação Swagger

--- src/main/java/com/extratoPopular/interfaces/dto/AuthResponse.java
+++ src/main/java/com/extratoPopular/interfaces/dto/AuthResponse.java
@@ -1,4 +1,11 @@
 package com.extratoPopular.interfaces.dto;
 
-public record AuthResponse() {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Resposta de autenticação contendo o token JWT")
+public record AuthResponse(
+
+        @Schema(description = "Token JWT gerado para a sessão", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
+        String token
+) {
 }
